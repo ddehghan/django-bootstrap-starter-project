5326ee6d4a9d3acd236e9354d555bb2603b7f7ca
David Dehghan <ddehghan@gmail.com>
2013-02-25 21:47:40 -0800

    put back the good urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,17 +1,19 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myproject.views.home', name='home'),
-    # url(r'^myproject/', include('myproject.foo.urls')),
+                       # Examples:
+                       # url(r'^$', 'myproject.views.home', name='home'),
+                       url(r'', include('social_auth.urls')),
+                       (r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       url(r'^$', 'website.views.home', name='home'),
+                       url(r'^private$', 'website.views.private', name='private'),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+                       # Admin site
+                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       url(r'^admin/', include(admin.site.urls)), )
