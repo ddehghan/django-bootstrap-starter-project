062acd7610fa98a71071e26367848f74b9e7b05d
David Dehghan <ddehghan@gmail.com>
2013-02-26 03:39:29 -0800

    new content

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -12,8 +13,14 @@ urlpatterns = patterns('',
                        (r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 
                        url(r'^$', 'website.views.home', name='home'),
+                       url(r'^index$', 'website.views.index', name='index'),
                        url(r'^private$', 'website.views.private', name='private'),
 
+                       url("^mission", direct_to_template, {"template": "mission.html"}, name="mission"),
+                       url("^programs", direct_to_template, {"template": "programs.html"}, name="programs"),
+                       url("^drawings", direct_to_template, {"template": "drawings.html"}, name="drawings"),
+
                        # Admin site
-                       url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-                       url(r'^admin/', include(admin.site.urls)), )
+                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # url(r'^admin/', include(admin.site.urls)),
+                       )
