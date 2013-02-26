74c225a62393d467a766dc2b815def98fbaf57fe
David Dehghan <ddehghan@gmail.com>
2013-02-25 18:40:43 -0800

    context handler

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.utils import simplejson
 from django.http import HttpResponse
+from django.template import RequestContext
 
 from coffin.views.generic.simple import direct_to_template
 from coffin.shortcuts import get_object_or_404
@@ -9,12 +10,11 @@ from coffin.shortcuts import get_object_or_404
 def home(request):
     return direct_to_template(request, 'index.html', {
         'a': 'a',
-    })
+    }, context_instance=RequestContext(request))
 
 
 @login_required
 def private(request):
     return direct_to_template(request, 'private.html', {
         'a': 'a',
-    })
-
+    }, context_instance=RequestContext(request))
