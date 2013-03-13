6a41778aa5972f2e1f280dfdce9d8a6cab5941de
David Dehghan <ddehghan@gmail.com>
2013-03-13 12:42:18 -0700

    remove from SC

--- myproject/settings_local.heroku.sh
+++ myproject/settings_local.heroku.sh
@@ -1,15 +1,29 @@
-# Heroku
+#
+#  _____                      _      _____                          _ _
+# |  __ \                    | |    / ____|                        (_) |
+# | |  | | ___    _ __   ___ | |_  | |     ___  _ __ ___  _ __ ___  _| |_
+# | |  | |/ _ \  | '_ \ / _ \| __| | |    / _ \| '_ ` _ \| '_ ` _ \| | __|
+# | |__| | (_) | | | | | (_) | |_  | |___| (_) | | | | | | | | | | | | |_
+# |_____/ \___/  |_| |_|\___/ \__|  \_____\___/|_| |_| |_|_| |_| |_|_|\__|
+#
+#
 
-heroku config:add S3_KEY=AKIAIPZ5KTYJMCETOL5A
-heroku config:add S3_SECRET=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
+# Heroku Settings
+
+heroku config:add S3_KEY=AKIAIONKG56MOQZQYQVQ
+heroku config:add S3_SECRET=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
 heroku config:add FACEBOOK_APP_ID=113304462166378
 heroku config:add FACEBOOK_API_SECRET=b96e196bcf48e288f1f9ef1b3c1f6047
 heroku config:add GOOGLE_OAUTH2_CLIENT_ID=148058405594-umc70ha0efj68ua5m8b5nmgrq4c2osm5.apps.googleusercontent.com
 heroku config:add GOOGLE_OAUTH2_CLIENT_SECRET=zbu4FvV65gX7yIKNicCGZJvI
 
-export S3_KEY=AKIAIPZ5KTYJMCETOL5A
-export S3_SECRET=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
+
+# Local settings
+
+export S3_KEY=AKIAIONKG56MOQZQYQVQ
+export S3_SECRET=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
 export FACEBOOK_APP_ID=113304462166378
 export FACEBOOK_API_SECRET=b96e196bcf48e288f1f9ef1b3c1f6047
 export GOOGLE_OAUTH2_CLIENT_ID=148058405594-umc70ha0efj68ua5m8b5nmgrq4c2osm5.apps.googleusercontent.com
 export GOOGLE_OAUTH2_CLIENT_SECRET=zbu4FvV65gX7yIKNicCGZJvI
+
