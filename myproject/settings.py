39dddacb8438fd7eb8f7bf94cc142ef1c92a1dff
David Dehghan <ddehghan@gmail.com>
2013-03-17 11:59:27 -0700

    cleanup

--- myproject/settings.py
+++ myproject/settings.py
@@ -251,5 +251,3 @@ DATABASES = {'default': dj_database_url.config(default='sqlite:/data.db')}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-print SECRET_KEY
